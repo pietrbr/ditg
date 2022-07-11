f5ac0915e8d61f46d5e560fa70da273525ef703e
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-07-11 17:31:52 -0400

    Change ports for controlled traffic

--- tshark/collector.sh
+++ tshark/collector.sh
@@ -2,4 +2,4 @@
 
 DATETIME=`date +"%Y_%m_%d_%H_%M_%S"`
 
-tshark -i lxdbr0 -a duration:10 -w daje --color -P -w tshark_log_$DATETIME
+tshark -i lxdbr0 -a duration:10 --color -P -w tshark_log_$DATETIME
