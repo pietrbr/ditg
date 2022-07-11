41a8a5f71f9206661d9ea2c53a8340e3af8b84cc
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-07-11 12:07:38 -0400

    Move everythong inside bin

--- runner.sh
+++ runner.sh
@@ -3,8 +3,8 @@
 DATETIME=`date +"%Y_%m_%d_%H_%M_%S"`
 
 # script_file is a script containing the commands
-./bin/ITGSend script_file -l log_$DATETIME
+./ITGSend script_file -l logs/log_$DATETIME
 
 echo 'Complete!'
 
-./bin/ITGDec log_$DATETIME
+./ITGDec logs/log_$DATETIME
--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir bin
+mkdir bin/logs
 cd src
 make -j4
 cd ../bin
@@ -11,3 +12,4 @@ cp ../src/ITGRecv/ITGRecv ./
 cp ../src/ITGManager/ITGManager ./
 
 cat "-a 10.244.105.38 -N 2.175 0.05 -n 115 35 -j 1 -t 1000 -T TCP" > ../script_file
+mv script_file runner-sh -t bin
