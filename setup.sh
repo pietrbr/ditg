ed38e4671e0aa954a5fb70ffa47260f97403ff8e
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-07-11 12:12:49 -0400

    ..

--- setup.sh
+++ setup.sh
@@ -11,5 +11,4 @@ cp ../src/ITGLog/ITGLog ./
 cp ../src/ITGRecv/ITGRecv ./
 cp ../src/ITGManager/ITGManager ./
 
-cat "-a 10.244.105.38 -N 2.175 0.05 -n 115 35 -j 1 -t 1000 -T TCP" > ../bin/script_file
-mv runner.sh bin/
+mv script_file runner.sh -t bin/
