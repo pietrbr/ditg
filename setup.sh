9ec6714b999d5fa502be14f32ca3da6b80fd0986
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-07-11 12:14:42 -0400

    ...

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 mkdir bin
 mkdir bin/logs
+mv script_file runner.sh -t bin/
+
 cd src
 make -j4
 cd ../bin
@@ -11,4 +13,3 @@ cp ../src/ITGLog/ITGLog ./
 cp ../src/ITGRecv/ITGRecv ./
 cp ../src/ITGManager/ITGManager ./
 
-mv script_file runner.sh -t bin/
