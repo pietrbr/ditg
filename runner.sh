847cb4d5872e2b3a26bacd68b29f83d5d01e989d
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-07-11 14:01:05 -0400

    ..

--- runner.sh
+++ runner.sh
@@ -3,8 +3,8 @@
 DATETIME=`date +"%Y_%m_%d_%H_%M_%S"`
 
 # script_file is a script containing the commands
-./ITGSend script_file -l logs/log_$DATETIME
+./ITGSend script_file -l logs/ditg_log_$DATETIME
 
 echo 'Complete!'
 
-./ITGDec logs/log_$DATETIME
+./ITGDec logs/ditg_log_$DATETIME
