b1bb1682627217ecc7600a42bcd248a9c0e8dc4b
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-07-12 10:46:58 -0400

    Correct typo

--- tshark/collector.sh
+++ tshark/collector.sh
@@ -2,7 +2,7 @@
 if [[ $# -eq 0 ]]; then
     DATETIME=`date +"%Y_%m_%d_%H_%M_%S"`
     FILENAME=tshark_log_$DATETIME
-elif [[ $# -eq 1 ]]: then
+elif [[ $# -eq 1 ]]; then
     FILENAME=tshark_log_$1
 fi
 
