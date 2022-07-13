f440ede55e39485bf567085298b1673d3ca51fd2
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-07-13 12:01:41 -0400

    .

--- bin/runner.sh
+++ bin/runner.sh
@@ -10,16 +10,16 @@ case $1 in
 
   bs__bg_cl)
     echo "Starting base station with background traffic and command_long"
-    ./ITGSend $1 -l logs/ditg_log_$DATETIME_bs
+    ./ITGSend $1 -l logs/ditg_log_$DATETIME\_bs
     echo "Complete!"
-    ./ITGDec logs/ditg_log_$DATETIME_bs
+    ./ITGDec logs/ditg_log_$DATETIME\_bs
     ;;
 
   ue__bg_ca)
     echo "Starting user equipment with background traffic and command_ack"
-    ./ITGSend $1 -l logs/ditg_log_$DATETIME_ue
+    ./ITGSend $1 -l logs/ditg_log_$DATETIME\_ue
     echo "Complete!"
-    ./ITGDec logs/ditg_log_$DATETIME_ue
+    ./ITGDec logs/ditg_log_$DATETIME\_ue
     ;;
 
   *)
