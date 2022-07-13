16182fd89c06343a7e1dc34d72146c6081e8a057
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-07-13 11:57:24 -0400

    Update runner files

--- bin/runner.sh
+++ bin/runner.sh
@@ -10,14 +10,14 @@ case $1 in
 
   bs__bg_cl)
     echo "Starting base station with background traffic and command_long"
-    ./ITGSend scripts/$1 -l logs/ditg_log_$DATETIME_bs
+    ./ITGSend $1 -l logs/ditg_log_$DATETIME_bs
     echo "Complete!"
     ./ITGDec logs/ditg_log_$DATETIME_bs
     ;;
 
   ue__bg_ca)
     echo "Starting user equipment with background traffic and command_ack"
-    ./ITGSend scripts/$1 -l logs/ditg_log_$DATETIME_ue
+    ./ITGSend $1 -l logs/ditg_log_$DATETIME_ue
     echo "Complete!"
     ./ITGDec logs/ditg_log_$DATETIME_ue
     ;;
