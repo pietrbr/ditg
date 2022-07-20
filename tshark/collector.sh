e48998fe0b690bed67dc2ceb40866bb3bcb48673
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-07-20 18:28:55 -0400

    Change tshark file name to have correct pcapng extension

--- tshark/collector.sh
+++ tshark/collector.sh
@@ -3,7 +3,7 @@ if [[ $# -eq 0 ]]; then
     DATETIME=`date +"%Y_%m_%d_%H_%M_%S"`
     FILENAME=tshark_log_$DATETIME
 elif [[ $# -eq 1 ]]; then
-    FILENAME=tshark_log_$1
+    FILENAME=tshark_log_$1.pcapng
 fi
 
 tshark -i lxdbr0 -a duration:10 --color -P -w $FILENAME
