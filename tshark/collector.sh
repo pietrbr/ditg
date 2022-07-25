46c34f26eea6dc552fc60023eeb7c205ce5d32dd
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-07-25 14:23:49 -0400

    Update tshark/collector.sh script

--- tshark/collector.sh
+++ tshark/collector.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 if [[ $# -eq 0 ]]; then
     DATETIME=`date +"%Y_%m_%d_%H_%M_%S"`
-    FILENAME=tshark_log_$DATETIME
+    FILENAME=tshark_log_$DATETIME.pcapng
 elif [[ $# -eq 1 ]]; then
     FILENAME=tshark_log_$1.pcapng
 fi
 
 tshark -i lxdbr0 -a duration:10 --color -P -w $FILENAME
+# tshark -i tun_srsue -a duration:2750 --color -P -w $FILENAME
+# tshark -i srs_spgw_sgi -a duration:2750 --color -P -w $FILENAME
